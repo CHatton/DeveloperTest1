64ccb8642df639400e4daac3d89fcd863bbe610c
chatton <cianhatton@gmail.com>
2017-10-10 18:27:25 +0100

    made World class immutable

--- src/viagogo/worlds/World.java
+++ src/viagogo/worlds/World.java
@@ -8,38 +8,32 @@ import java.util.stream.Collectors;
 
 public class World {
 
-    private DistanceMetric distanceMetric;
+    private final DistanceMetric distanceMetric;
     private final Map<Point, Event> events;
 
-    public World(DistanceMetric distanceMetric) {
+    public World(final Map<Point, Event> events, final DistanceMetric distanceMetric) {
+        this.events = Collections.unmodifiableMap(events);
         this.distanceMetric = distanceMetric;
-        events = new HashMap<>();
     }
 
     public int distanceToEvent(Point start, Event event) {
-        Point eventPoint = getEventPoint(event);
-        return distanceMetric.distanceBetween(start, eventPoint);
+        Optional<Point> eventPoint = getEventPoint(event);
+        // negative number to indicate point doesn't exist.
+        return eventPoint.isPresent() ? distanceMetric.distanceBetween(start, eventPoint.get()) : -1;
     }
 
-    private Point getEventPoint(Event event) {
+    private Optional<Point> getEventPoint(Event event) {
         if (!events.containsValue(event)) {
             throw new IllegalArgumentException("Event [%s] is not in the world." + event);
         }
         for (Map.Entry<Point, Event> entry : events.entrySet()) {
             if (entry.getValue().equals(event)) {
-                return entry.getKey();
+                return Optional.of(entry.getKey());
             }
         }
-        return null;
+        return Optional.empty();
     }
 
-    public void setDistanceMetric(DistanceMetric distanceMetric) {
-        this.distanceMetric = distanceMetric;
-    }
-
-    public void addEventAt(Event event, Point point) {
-        events.put(point, event);
-    }
 
     public List<Event> getClosestEvents(Point point, int n) {
         // assuming you want to return all up to a number if the world doesn't contain enough points.
